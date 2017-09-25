aa7eb35e3ced4f5318917f09724626fdf4578b52
Beatriz Pacheco Pinero <bpacheco@alpha30.aulas.gsyc.urjc.es>
2017-09-25 20:47:16 +0200

    Implementados los puntos 4 y 5 de la práctica.

---
+++ calcoo.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+    """Esto es un ejemplo de clase que hereda de ClaseMadre"""
+
+    def __init__(self):
+        """Esto es el método inicializador"""
+        pass #esto no hace falta, no hace nada
+    
+    def suma(self, valor1, valor2):
+        """Sumo valor1 y valor2"""
+        return valor1 + valor2
+    
+    def resta(self, valor1, valor2):
+        """Sumo valor1 y valor2"""
+        return valor1 - valor2
+
+
+if __name__ == "__main__":
+  
+    if len(sys.argv) != 4:
+        sys.exit('Úsalo así: python3 calcoo.py operando1 operador operando2')
+
+    operador = sys.argv[2]
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    calc = Calculadora()
+    
+    if operador == 'suma':
+        print(calc.suma(operando1, operando2))
+    elif operador == 'resta':
+        print(calc.resta(operando1, operando2))
+    else:
+        print('Usa: suma, resta')
+
+
+
+
+
+
+
+#instanciar es pasar de algo abstracto a algo concreto
--- esqueleto-oo.py
+++ esqueleto-oo.py
@@ -2,13 +2,30 @@
 # -*- coding: utf-8 -*-
 
 class Clase(ClaseMadre):
-  "Esto es un ejemplo de clase que hereda de ClaseMadre"
+  """Esto es un ejemplo de clase que hereda de ClaseMadre"""
 
-  def __init__(self, valor):
-    "Esto es el método iniciliazador"
-    self.atributo = valor
+  def __init__(self, valor1, valor2):
+    """Esto es el método iniciliazador"""
+    self.valor1 = valor1
+    self.valor2 = valor2
+    
+  def suma(self):
+    """Sumo valor1 y valor2"""
+    res = self.valor1 + self.valor2
 
 if __name__ == "__main__":
-  objeto = Clase("pepe") # Creo un objeto de la clase Clase
-                         # y le paso el valor pepe para su
-                         # atributo en la inicialización
+  objeto = Clase("pepe", "48544682K")   # Creo un objeto de la clase Clase
+                                        # y le paso el valor pepe para su
+                                        # atributo en la inicialización
+                                        
+                                        #Le paso valor1 y valor2
+  
+  objeto2 = Clase("maria", "4873736976L")
+  objeto2.suma()
+                         
+                         
+                         
+                         
+                         
+                         
+#instanciar es pasar de algo abstracto a algo concreto
