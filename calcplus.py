687d5b5cc7557df025c835f7c29a269e0520d53f
Beatriz Pacheco Pinero <bpacheco@alpha20.aulas.gsyc.urjc.es>
2017-09-28 19:21:53 +0200

    Funciona, aunque tengo algunas dudas como ordenas codigo

--- calcplus.py
+++ calcplus.py
@@ -5,10 +5,9 @@ import sys
 import csv
 
 from calcoohija import CalculadoraHija
+    
 
 if __name__ == "__main__":
-  
-   
 
     if len(sys.argv) != 2:
         sys.exit('Úsalo así: python3 calcplus.py <fichero>')
@@ -19,19 +18,25 @@ if __name__ == "__main__":
     with open(fich) as csvarchivo:
         entrada = csv.reader(csvarchivo)
         for linea in entrada:
-            operandos = linea[1:]
+            
             print(linea)
-            print(operandos)
-            op1 = int(operandos[0])
-            op2 = int(operandos[1])
+            #print(operandos) lo uso o uso lineas?
+            #Lo que decia sergio de hacer el for fuera
+            
+            resultado = int(linea[1])
+            
             if linea[0] == 'suma':
-                resultado = calc.suma(op1, op2)
+                for num in linea[2:]:
+                    resultado = calc.suma(resultado, int(num))
             elif linea[0] == "resta":
-                resultado = calc.resta(op1, op2)
+                for num in linea[2:]:
+                    resultado = calc.resta(resultado, int(num))
             elif linea[0] == "multiplica":
-                resultado = calc.multiplica(op1, op2)
+                for num in linea[2:]:
+                    resultado = calc.multiplica(resultado, int(num))
             elif linea[0] == "divide":
-                resultado = calc.divide(op1, op2)
+                for num in linea[2:]:
+                    resultado = calc.divide(resultado, int(num))
             else:
                 print('suma, resta, multiplica o divide')
                       
