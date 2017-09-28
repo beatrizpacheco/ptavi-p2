5b917b9925fb66ca2d6a981f9438eaa99341e44d
Beatriz Pacheco Pinero <bpacheco@alpha20.aulas.gsyc.urjc.es>
2017-09-28 19:43:01 +0200

    He sustituido el calcplus por el calcplusplus (habia usado el csv sin darme cuenta de que era el punto anterior) y he hecho el calcplus sin csv. Implementado puntos 6 y 7

--- calcplus.py
+++ calcplus.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import csv
 
 from calcoohija import CalculadoraHija
     
@@ -12,33 +11,33 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit('Úsalo así: python3 calcplus.py <fichero>')
 
-    fich = sys.argv[1]
+    fich = open(sys.argv[1], 'r')
     calc = CalculadoraHija()
     
-    with open(fich) as csvarchivo:
-        entrada = csv.reader(csvarchivo)
-        for linea in entrada:
-            
-            print(linea)
-            #print(operandos) lo uso o uso lineas?
-            #Lo que decia sergio de hacer el for fuera
-            
-            resultado = int(linea[1])
-            
-            if linea[0] == 'suma':
-                for num in linea[2:]:
-                    resultado = calc.suma(resultado, int(num))
-            elif linea[0] == "resta":
-                for num in linea[2:]:
-                    resultado = calc.resta(resultado, int(num))
-            elif linea[0] == "multiplica":
-                for num in linea[2:]:
-                    resultado = calc.multiplica(resultado, int(num))
-            elif linea[0] == "divide":
-                for num in linea[2:]:
-                    resultado = calc.divide(resultado, int(num))
-            else:
-                print('suma, resta, multiplica o divide')
-                      
-            print(resultado)
+    for cadena in fich:
+    
+        linea = cadena[:-1].split(',')#El linea[:-1] es para quitar el \n
+               
+        print(linea)
+        #print(operandos) lo uso o uso lineas?
+        #No sé si hacer el for fuera o dentro
+        
+        resultado = int(linea[1])
+        
+        if linea[0] == 'suma':
+            for num in linea[2:]:
+                resultado = calc.suma(resultado, int(num))
+        elif linea[0] == "resta":
+            for num in linea[2:]:
+                resultado = calc.resta(resultado, int(num))
+        elif linea[0] == "multiplica":
+            for num in linea[2:]:
+                resultado = calc.multiplica(resultado, int(num))
+        elif linea[0] == "divide":
+            for num in linea[2:]:
+                resultado = calc.divide(resultado, int(num))
+        else:
+            print('suma, resta, multiplica o divide')
+                  
+        print(resultado)
             
---
+++ calcplusplus.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+
+from calcoohija import CalculadoraHija
+    
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        sys.exit('Úsalo así: python3 calcplus.py <fichero>')
+
+    fich = sys.argv[1]
+    calc = CalculadoraHija()
+    
+    with open(fich) as csvarchivo:
+        entrada = csv.reader(csvarchivo)
+        for linea in entrada:
+            
+            print(linea)
+            #print(operandos) lo uso o uso lineas?
+            #No sé si hacer el for fuera o dentro
+            
+            resultado = int(linea[1])
+            
+            if linea[0] == 'suma':
+                for num in linea[2:]:
+                    resultado = calc.suma(resultado, int(num))
+            elif linea[0] == "resta":
+                for num in linea[2:]:
+                    resultado = calc.resta(resultado, int(num))
+            elif linea[0] == "multiplica":
+                for num in linea[2:]:
+                    resultado = calc.multiplica(resultado, int(num))
+            elif linea[0] == "divide":
+                for num in linea[2:]:
+                    resultado = calc.divide(resultado, int(num))
+            else:
+                print('suma, resta, multiplica o divide')
+                      
+            print(resultado)
+            
